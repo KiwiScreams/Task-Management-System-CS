f89996a8441bb91c79efb712354c5dfaefc52b85
KiwiScreams <screamskiwi3@gmail.com>
2026-05-02 21:51:04 +0400

    Log Model

--- Models/Log.cs
+++ Models/Log.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementSystem.Enums;
 
-namespace TaskManagementSystem.Models
+namespace TaskManagementSystem.Models;
+
+public class Log
 {
-    internal class Log
-    {
-    }
+    public Guid Id { get; set; }
+    public Guid TaskId { get; set; }
+    public ActionType ActionType { get; set; }
+    public string Message { get; set; }
+    public DateTime TimesTamp { get; set; }
 }
