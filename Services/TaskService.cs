2b870ad36915921117beb441e72c3c59f4dae431
KiwiScreams <screamskiwi3@gmail.com>
2026-05-02 22:18:08 +0400

    Task Services

--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
+using TaskManagementSystem.Models;
 
-namespace TaskManagementSystem.Services
+namespace TaskManagementSystem.Services;
+
+public class TaskService
 {
-    internal class TaskService
+    private const string TasksFilePath = "Tasks.json";
+    private const string LogFilePath = "Logs.json";
+
+    public void WriteTasks(List<TaskManagementSystem.Models.TaskItem> tasks)
     {
+        string TaskJson = JsonSerializer.Serialize(tasks, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(TasksFilePath, TaskJson);
+    }
+    public List<TaskManagementSystem.Models.TaskItem> ReadTasks()
+    {
+        if (!File.Exists(TasksFilePath))
+        {
+            return new List<TaskItem>();
+        }
+
+        string TaskJson = File.ReadAllText(TasksFilePath);
+
+        if (string.IsNullOrWhiteSpace(TaskJson))
+        {
+            return new List<TaskItem>();
+        }
+
+        return JsonSerializer.Deserialize<List<TaskItem>>(TaskJson)
+               ?? new List<TaskItem>();
     }
 }
