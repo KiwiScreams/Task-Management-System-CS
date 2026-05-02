2842e0f05f51b51fccbcecba54c072b1e4ba5546
KiwiScreams <screamskiwi3@gmail.com>
2026-05-02 22:42:28 +0400

    Log Service: Add Log

--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -10,11 +10,13 @@ namespace TaskManagementSystem.Services;
 public class TaskService
 {
     private FileService fileService = new FileService();
+    // Task management
     public void CreateTask(string title, string description)
     {
         List<TaskItem> tasks = fileService.ReadTasks();
 
         TaskItem newTask = new TaskItem();
+
         newTask.Id = Guid.NewGuid();
         newTask.Title = title;
         newTask.Description = description;
@@ -22,6 +24,24 @@ public class TaskService
         newTask.CreatedAt = DateTime.Now;
         tasks.Add(newTask);
         fileService.WriteTasks(tasks);
+
         Console.WriteLine("Task created successfully!");
+        AddLog(newTask.Id, $"Task created: {newTask.Title}");
+    }
+    // Logs management
+    public void AddLog(Guid taskId, string message)
+    {
+        List<Log> logs = fileService.ReadLogs();
+
+        Log newLog = new Log();
+
+        newLog.Id = Guid.NewGuid();
+        newLog.TaskId = taskId;
+        newLog.ActionType = ActionType.Created;
+        newLog.Message = message;
+        newLog.TimesTamp = DateTime.Now;
+        logs.Add(newLog);
+        fileService.WriteLogs(logs);
+        Console.WriteLine(message);
     }
 }
