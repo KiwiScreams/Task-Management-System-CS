36c07d23a4af7fce8cdcad7f8f84af1ce184d648
KiwiScreams <screamskiwi3@gmail.com>
2026-05-02 22:23:57 +0400

    Log Services

--- Services/FileService.cs
+++ Services/FileService.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
+using TaskManagementSystem.Models;
 
-namespace TaskManagementSystem.Services
+namespace TaskManagementSystem.Services;
+
+public class FileService
 {
-    internal class FileService
+    private const string TasksFilePath = "Tasks.json";
+    private const string LogFilePath = "Logs.json";
+
+    // Tasks management
+    public void WriteTasks(List<TaskManagementSystem.Models.TaskItem> tasks)
     {
+        string TaskJson = JsonSerializer.Serialize(tasks, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(TasksFilePath, TaskJson);
+    }
+    public List<TaskManagementSystem.Models.TaskItem> ReadTasks()
+    {
+        if (!File.Exists(TasksFilePath))
+        {
+            return new List<TaskItem>();
+        }
+
+        string TaskJson = File.ReadAllText(TasksFilePath);
+
+        if (string.IsNullOrWhiteSpace(TaskJson))
+        {
+            return new List<TaskItem>();
+        }
+
+        return JsonSerializer.Deserialize<List<TaskItem>>(TaskJson)
+               ?? new List<TaskItem>();
+    }
+
+    // Logs management
+    public void WriteLogs(List<Log> logs)
+    {
+        string LogJson = JsonSerializer.Serialize(logs, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(LogFilePath, LogJson);
+    }
+    public List<TaskManagementSystem.Models.Log> ReadLogs()
+    {
+        if (!File.Exists(LogFilePath))
+        {
+            return new List<Log>();
+        }
+
+        string LogJson = File.ReadAllText(LogFilePath);
+
+        if (string.IsNullOrWhiteSpace(LogJson))
+        {
+            return new List<Log>();
+        }
+
+        return JsonSerializer.Deserialize<List<Log>>(LogJson)
+               ?? new List<Log>();
     }
 }
--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -8,29 +8,5 @@ namespace TaskManagementSystem.Services;
 
 public class TaskService
 {
-    private const string TasksFilePath = "Tasks.json";
-    private const string LogFilePath = "Logs.json";
 
-    public void WriteTasks(List<TaskManagementSystem.Models.TaskItem> tasks)
-    {
-        string TaskJson = JsonSerializer.Serialize(tasks, new JsonSerializerOptions { WriteIndented = true });
-        File.WriteAllText(TasksFilePath, TaskJson);
-    }
-    public List<TaskManagementSystem.Models.TaskItem> ReadTasks()
-    {
-        if (!File.Exists(TasksFilePath))
-        {
-            return new List<TaskItem>();
-        }
-
-        string TaskJson = File.ReadAllText(TasksFilePath);
-
-        if (string.IsNullOrWhiteSpace(TaskJson))
-        {
-            return new List<TaskItem>();
-        }
-
-        return JsonSerializer.Deserialize<List<TaskItem>>(TaskJson)
-               ?? new List<TaskItem>();
-    }
 }
