820dc1ef5b4a6c04931a43a2570338664d646ad5
KiwiScreams <screamskiwi3@gmail.com>
2026-05-02 21:48:40 +0400

    Task Model

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TaskManagementSystem.Models
+namespace TaskManagementSystem.Models;
+
+public class TaskItem
 {
-    internal class TaskItem
-    {
-    }
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TaskStatus Status { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
