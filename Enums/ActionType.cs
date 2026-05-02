979dc9c3f8c6792877abb2679f3647b47cb85f6d
KiwiScreams <screamskiwi3@gmail.com>
2026-05-02 21:42:48 +0400

    Task Status

---
+++ Enums/ActionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Enums
+{
+    internal class ActionType
+    {
+    }
+}
---
+++ Enums/TaskStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskManagementSystem.Enums;
+public enum TaskStatus
+{
+    ToDo,
+    InProgress,
+    Testing,
+    Done
+}
